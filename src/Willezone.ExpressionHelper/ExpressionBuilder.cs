054b13b7f8826f533f5319fb75c1d94b210dfd90
Boris Wilhelms <mail@wille-zone.de>
2015-02-15 18:24:59 +0100

    Refactored CreateMemberAccess and simplified expression creation.

--- src/Willezone.ExpressionHelper/ExpressionBuilder.cs
+++ src/Willezone.ExpressionHelper/ExpressionBuilder.cs
@@ -13,48 +13,29 @@ namespace Willezone.ExpressionHelper
             Contract.Requires<ArgumentException>(!String.IsNullOrWhiteSpace(memberPath), "memberPath must have a value");
             var memberPathParts = SplitMemberPath(memberPath);
 
-            MemberExpression memberExpression = null;
             var parameter = Expression.Parameter(typeof(T), "Model");
-
+            Type parentType = typeof(T);
+            Expression parentExpression = parameter;
+             
             for (int i = 0; i < memberPathParts.Length; i++)
             {
-                if (i == 0)
+                MemberInfo memberInfo =
+                    parentType.GetFields(BindingFlags.Public | BindingFlags.Instance).Cast<MemberInfo>()
+                    .Concat(parentType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                    .FirstOrDefault(m => m.Name == memberPathParts[i]);
+
+                if (memberInfo == null)
                 {
-                    var type = typeof(T);
-
-                    MemberInfo memberInfo =
-                        type.GetFields(BindingFlags.Public | BindingFlags.Instance).Cast<MemberInfo>()
-                        .Concat(type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
-                        .FirstOrDefault(m => m.Name == memberPathParts[i]);
-
-                    if (memberInfo == null)
-                    {
-                        throw new MissingMemberException(type.Name, memberPath);
-                    }
-
-                    memberExpression = Expression.MakeMemberAccess(parameter, memberInfo);
+                    throw new MissingMemberException(parentType.Name, memberPathParts[i]);
                 }
-                else
-                {
-                    var type = memberExpression.Member.DeclaringType;
 
-                    MemberInfo memberInfo =
-                        type.GetFields(BindingFlags.Public | BindingFlags.Instance).Cast<MemberInfo>()
-                        .Concat(type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
-                        .FirstOrDefault(m => m.Name == memberPathParts[i]);
-
-                    if (memberInfo == null)
-                    {
-                        throw new MissingMemberException(type.Name, memberPath);
-                    }
-
-                    memberExpression = Expression.MakeMemberAccess(memberExpression, memberInfo);
-                }
+                parentType = memberInfo.DeclaringType;
+                parentExpression = Expression.MakeMemberAccess(parentExpression, memberInfo);
             }
 
-            var convertExpression = Expression.Convert(memberExpression, typeof(object));
-
+            var convertExpression = Expression.Convert(parentExpression, typeof(object));
             var lambda = Expression.Lambda<Func<T, object>>(convertExpression, parameter);
+
             return lambda.Compile();
         }
 
